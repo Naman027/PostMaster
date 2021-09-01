6647b29e44582a635d687ec53e03dbc5ac0d88d6
NamanGoyal07 <naman18@iitg.ac.in>
2021-09-02 00:08:16 +0530

    Json Parameters Box done

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-console.log("My Project");
+//Hide the parameters box initially
+let parametersBox = document.getElementById('parametersBox');
+parametersBox.style.display = 'none';
+
+// if the user clicks on params box hide the json box
+let paramsRadio = document.getElementById('paramsRadio');
+paramsRadio.addEventListener('click',()=>{
+    document.getElementById('requestJsonBox').style.display = 'none';
+    document.getElementById('parametersBox').style.display = 'block';
+});
+
+// if the user clicks on json box hide the params box
+let jsonRadio = document.getElementById('jsonRadio');
+jsonRadio.addEventListener('click',()=>{
+    document.getElementById('parametersBox').style.display = 'none';
+    document.getElementById('requestJsonBox').style.display = 'block';
+});
+
 
 
 
