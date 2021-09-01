79562a1f09e705ca5af79c2fc03d21b341eed99d
NamanGoyal07 <naman18@iitg.ac.in>
2021-09-02 00:16:09 +0530

    Adding + button params

--- index.js
+++ index.js
@@ -2,20 +2,41 @@
 let parametersBox = document.getElementById('parametersBox');
 parametersBox.style.display = 'none';
 
+let addedParamsCount = 2;
+
 // if the user clicks on params box hide the json box
 let paramsRadio = document.getElementById('paramsRadio');
-paramsRadio.addEventListener('click',()=>{
+paramsRadio.addEventListener('click', () => {
     document.getElementById('requestJsonBox').style.display = 'none';
     document.getElementById('parametersBox').style.display = 'block';
 });
 
 // if the user clicks on json box hide the params box
 let jsonRadio = document.getElementById('jsonRadio');
-jsonRadio.addEventListener('click',()=>{
+jsonRadio.addEventListener('click', () => {
     document.getElementById('parametersBox').style.display = 'none';
     document.getElementById('requestJsonBox').style.display = 'block';
 });
 
+// adding more params when user clicks "+" button
+
+let plusButton = document.getElementById('addParam');
+plusButton.addEventListener('click', () => {
+    let params = document.getElementById('params');
+    let string = `<div class="form-row">
+                    <label for="url" class="col-sm-2 col-form-label">Parameter ${addedParamsCount}</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" id="parameterKey${addedParamsCount}" placeholder="Enter Parameter ${addedParamsCount} Key">
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" id="parameterValue${addedParamsCount}" placeholder="Enter Parameter ${addedParamsCount} Value">
+                    </div>
+                </div>`;
+    addedParamsCount++;
+
+    
+    
+});
 
 
 
